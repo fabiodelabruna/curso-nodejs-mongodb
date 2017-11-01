e91795d8431d08d94737446fe9881e310dc67a08
Fabio Dela Bruna <fabio.bruna@digitro.com.br>
2017-11-01 14:04:39 -0200

    Criação dos primeiros models e inclusão no autoload do consign.

--- portal-noticias/app/routes/noticia.js
+++ portal-noticias/app/routes/noticia.js
@@ -2,10 +2,11 @@
 module.exports = function(app) {
 
     app.get('/noticia', function(req, res) {
-        
+
         var connection = app.config.dbConnection();
-        
-        connection.query('select * from noticias where id_noticia = 2', function(error, result) {
+        var noticiasModel = app.app.models.noticiasModel;
+
+        noticiasModel.getNoticia(connection, function(error, result) {
             if (!error) {
                 res.render('noticias/noticia', { noticia: result[0] });
             }
--- portal-noticias/config/server.js
+++ portal-noticias/config/server.js
@@ -8,6 +8,7 @@ app.set('views', './app/views');
 consign()
     .include('app/routes') // adiciona as rotas no autoload
     .then('config/dbConnection.js') // adiciona o módulo de conexão no autoload
+    .then('app/models') // adiciona os models no autoload
     .into(app);
 
 module.exports = app;
