57f3a8ad1106dc6cc98134a4ef5386253b336627
Fabio Dela Bruna <fabio.bruna@digitro.com.br>
2017-11-03 16:33:07 -0200

    Exibindo notícias do banco após aplicação do layout.

--- portal-noticias/app/models/NoticiasDAO.js
+++ portal-noticias/app/models/NoticiasDAO.js
@@ -3,11 +3,11 @@ function NoticiasDAO(connection) {
 }
 
 NoticiasDAO.prototype.getNoticia = function (callback) {
-    this._connection.query('select * from noticias where id_noticia = 2', callback);
+    this._connection.query('select * from noticias where id_noticia = 10', callback);
 }
 
 NoticiasDAO.prototype.getNoticias = function (callback) {
-    this._connection.query('select * from noticias', callback);
+    this._connection.query('select * from noticias order by data_criacao desc', callback);
 }
 
 NoticiasDAO.prototype.getCincoUltimasNoticias = function(callback) {
