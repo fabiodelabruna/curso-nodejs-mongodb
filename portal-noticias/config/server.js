2d0e312a6669fc64d81bf36185e16360e24c3470
Fabio Dela Bruna <fabio.bruna@digitro.com.br>
2017-11-03 09:28:52 -0200

    Inclusão de dados no banco e instalação do body-parser.

--- portal-noticias/config/server.js
+++ portal-noticias/config/server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+// inclui o middleware body-parser
+app.use(bodyParser.urlencoded({ extend: true }));
 
 consign()
     .include('app/routes') // adiciona as rotas no autoload
