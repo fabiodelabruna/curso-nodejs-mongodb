86e0c68fedd6bc2f00fb9fd146477e268bf97404
Fabio Dela Bruna <fabiodelabruna@gmail.com>
2017-11-05 20:59:54 -0200

    Finalização do projeto de chat

--- multiroom-chat/app.js
+++ multiroom-chat/app.js
@@ -1,5 +1,31 @@
 var app = require('./config/server');
 
-app.listen(8000, function () {
-    console.log('OK');
-})
+var server = app.listen(8000, function () {
+    console.log('Servidor online...');
+});
+
+var io = require('socket.io').listen(server);
+app.set('io', io);
+
+// cria a conexão por websocket
+io.on('connection', function(socket) {
+    console.log('Usuário conectou!!!');
+
+    socket.on('disconnect', function() {
+        console.log('Usuário desconectou!!!');
+    });
+
+    socket.on('msgParaServidor', function(data) {
+        // Atualiza diálogo
+        var content = { apelido: data.apelido, mensagem: data.mensagem }
+        socket.emit('msgParaCliente', content);
+        socket.broadcast.emit('msgParaCliente', content);
+
+        if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            // Atualiza lista de participantes
+            socket.emit('participantesParaCliente', { apelido: data.apelido });
+            socket.broadcast.emit('participantesParaCliente', { apelido: data.apelido });
+        }
+    });
+
+});
--- multiroom-chat/app/controller/chat.js
+++ multiroom-chat/app/controller/chat.js
@@ -1,3 +1,24 @@
 module.exports.startChat = function (application, req, res) {
-    res.render("chat");
+    
+    var dadosForm = req.body;
+
+    req.assert('apelido', 'Você deve informar um nome ou apelido.').notEmpty();
+    req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres.').len(3, 15);
+
+    erros = req.validationErrors();
+
+    if (erros) {
+        res.render('index', { validacao: erros });
+        return;
+    }
+
+    application.get('io').emit(
+        'msgParaCliente',
+        {
+            apelido: dadosForm.apelido,
+            mensagem: 'Acabou de entrar no chat!'
+        }
+    );
+
+    res.render('chat', { dadosForm: dadosForm });
 };
