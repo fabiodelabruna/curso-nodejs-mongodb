eb4168df97b18f18b827353387a0de7c81edcb64
Fabio Dela Bruna <fabio.bruna@digitro.com.br>
2017-11-09 13:18:16 -0200

    Instalação e configuração do módulo express-session

--- mmorpg-got/app/controllers/index.js
+++ mmorpg-got/app/controllers/index.js
@@ -1,3 +1,20 @@
 module.exports.index = function(application, req, res) {
-    res.render('index');
+    res.render('index', { validacao: {} });
+}
+
+module.exports.autenticar = function(application, req, res) {
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'Usuário deve ser informado').notEmpty();
+    req.assert('senha', 'Senha deve ser informada').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        res.render('index', { validacao: erros });
+        return;
+    }
+
+    res.send('ok');
+
 }
